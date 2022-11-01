2f118d52cb90824dabd8a4bd3bfd273c721c3eb3
Tihohod2000 <dumanovskiy15@gmail.com>
2022-11-01 21:21:15 +0300

    kkdfjhsgljdflkgjsldf

--- calculate.c
+++ calculate.c
@@ -11,7 +11,7 @@ float Calculate(float Numeral, char Operation[4])
 	float SecondNumeral;
 	if(strncmp(Operation, "+", 1) == 0)
 	{
-	    printf("Второе слагаемое j: "); 
+	    printf("Второе слагаемое jjkasjdlkajs;ldkal;sd: "); 
 	    scanf("%f",&SecondNumeral); 
 	    return(Numeral + SecondNumeral);
 	}
